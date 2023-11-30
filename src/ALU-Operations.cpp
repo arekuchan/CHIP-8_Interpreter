43784da9ee809903f903b7131c1d5a4de844e2e1
arekuchan <kasiryeduncan@gmail.com>
2023-11-30 16:36:49 +1000

    Implemented (but not tested): shr_8XY6, subn_8XY7, shl_8XYE. Now all of the operations starting with '8' should be done.

--- src/ALU-Operations.cpp
+++ src/ALU-Operations.cpp
@@ -1,6 +1,7 @@
 #include "ALU-Operations.h"
 #include "registers.h"
 
+// TODO : move this opcode to another file, it isn't an ALU operation
 void assign_8XY0(std::int8_t vXAddr, std::int8_t vYAddr) {
     auto& registerMap = Registers::registerMap;
 
@@ -35,16 +36,40 @@ void add_8XY4(std::int8_t vXAddr, std::int8_t vYAddr) {
 
     // detect carry out by checking if 9th bit (left from MSB in 8 bit) is set
     bool carryOut = (0x1 << (9 - 1)) & additionResult;
-    carryFlagRegister = carryOut ? 0x1 : 0x0;
+    carryFlagRegister = carryOut ? 0x01 : 0x00;
 
     // the top half should get truncated
     vX = (int8_t) additionResult;
 }
 
-void minus_8XY5(std::int8_t vXaddr, std::int8_t vYAddr) {
+void minus_8XY5(std::int8_t vXAddr, std::int8_t vYAddr) {
     int8_t& carryFlagRegister = Registers::get_carry_flag_register();
-    auto&& [vX, vY] = Registers::get_vx_and_vy(vXaddr, vYAddr);
+    auto&& [vX, vY] = Registers::get_vx_and_vy(vXAddr, vYAddr);
 
-    carryFlagRegister = vX > vY ? 0x1 : 0x0;
+    carryFlagRegister = vX > vY ? 0x01 : 0x00;
     vX -= vY;
+}
+
+void shr_8XY6(std::int8_t vXAddr, std::int8_t vYAddr) {
+    int8_t& carryFlagRegister = Registers::get_carry_flag_register();
+    auto&& vX = Registers::registerMap.at(vXAddr);
+
+    carryFlagRegister = vX & 0x01 ? 0x01 : 0x00;
+    vX >>= 1;
+}
+
+void subn_8XY7(std::int8_t vXAddr, std::int8_t vYAddr) {
+    int8_t& carryFlagRegister = Registers::get_carry_flag_register();
+    auto&& [vX, vY] = Registers::get_vx_and_vy(vXAddr, vYAddr);
+
+    carryFlagRegister = vY > vX ? 0x01 : 0x00;
+    vX = vY - vX;
+}
+
+void shl_8XYE(std::int8_t vXAddr, std::int8_t vYAddr) {
+    int8_t& carryFlagRegister = Registers::get_carry_flag_register();
+    auto&& vX = Registers::registerMap.at(vXAddr);
+
+    carryFlagRegister = vX & 0x80 ? 0x01 : 0x00;
+    vX <<= 1;
 }
--- src/ALU-Operations.h
+++ src/ALU-Operations.h
@@ -13,4 +13,12 @@ void xor_8XY3(std::int8_t, std::int8_t);
 
 void add_8XY4(std::int8_t, std::int8_t);
 
+void minus_8XY5(std::int8_t, std::int8_t);
+
+void shr_8XY6(std::int8_t, std::int8_t);
+
+void subn_8XY7(std::int8_t, std::int8_t);
+
+void shl_8XYE(std::int8_t, std::int8_t);
+
 #endif
