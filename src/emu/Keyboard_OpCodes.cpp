1485d67f98a09740284dac52aae0ac92bb744e3c
arekuchan <kasiryeduncan@gmail.com>
2024-01-15 13:06:12 +1000

    Deleting these files, keyboard opcodes are implemented in control_flow.cpp

--- src/emu/Keyboard_OpCodes.cpp
+++
@@ -1,7 +0,0 @@
-#include "Keyboard_OpCodes.hpp"
-
-bool kse_EX9E(std::int8_t vXID) {
-    int8_t vXRegVal = Registers::variableRegistersMap.at(vXID);
-
-    // TODO : finish this
-}
--- src/emu/Keyboard_OpCodes.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef CHIP_8_KEYBOARD_OPCODES_H
-#define CHIP_8_KEYBOARD_OPCODES_H
-
-#include <cstdint>
-#include <cstdio>
-
-#include "registers.hpp"
-
-#endif
